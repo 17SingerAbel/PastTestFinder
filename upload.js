61c70c93464ab3e015d11b50647ce9b922e6fdbe
yixuannliu <yixuann.liu.utoronto@gmail.com>
2018-11-06 13:44:27 -0500

    add nav search box function

--- upload.js
+++ upload.js
@@ -1,7 +1,45 @@
-$( "#narbarSearch" ).on( "click", function() {
-  console.log( $( this ).text() );
+// $( "#narbarSearch" ).on( "click", function() {
+//   console.log( $( this ).text() );
+// });
+
+
+$(function() {
+  $("#searchButton").click(function(e) {
+    e.preventDefault();
+    // console.log( $( "#searchBox" ).val() );
+    checkInputAtSearchBox($( "#searchBox" ).val());
+  });
 });
 
+function checkInputAtSearchBox(courseName){
+	const dept = courseName.slice(0,3);
+	const courseNumber = courseName.slice(3);
+
+	if(isLetter(dept) && isThreeDigits(courseNumber)){
+		// const resultCourse = new Course(dept.toUpperCase(), courseNumber);
+		console.log(dept.toUpperCase()+courseNumber);
+	}
+}
+
+
+function isLetter(str) {
+  	return str.toLowerCase() != str.toUpperCase();
+}
+
+function isThreeDigits(str) {
+	const num = parseInt(str);
+
+	if(isNaN(num)){
+		$("#navbarErrorMessage").html("Not a course number");
+		return false;
+	}
+	if(num>=500 || num<100){
+		$("#navbarErrorMessage").html("Should be within 100-499");
+		return false;
+	}
+
+	return true;
+}
 
 
 const FileInfo = function(dept, courseNumber, year, term, file){

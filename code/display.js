98e43e1fa9715b1626e1d46ebddc60dfb1d7a305
Abel Sang <abelsang9683@gmail.com>
2018-11-06 20:28:11 -0500

    out layer done, remain sort and filter

---
+++ code/display.js
@@ -0,0 +1,18 @@
+// $(function() {
+//   $("#filterButton").click(function(e) {
+//     e.preventDefault();
+//     // console.log( $( "#searchBox" ).val() );
+//     checkInputAtSearchBox($( "#searchBox" ).val());
+//   });
+// });
+
+
+// function checkInputAtSearchBox(courseName){
+// 	const dept = courseName.slice(0,3);
+// 	const courseNumber = courseName.slice(3);
+
+// 	if(isLetter(dept) && isThreeDigits(courseNumber)){
+// 		// const resultCourse = new Course(dept.toUpperCase(), courseNumber);
+// 		console.log(dept.toUpperCase()+courseNumber);
+// 	}
+// }

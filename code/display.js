891a58520985bb18f6ddf08d90aacb60884d6c14
Abel Sang <abelsang9683@gmail.com>
2018-11-07 01:45:23 -0500

    add delete button

--- code/display.js
+++ code/display.js
@@ -1,18 +1,58 @@
-// $(function() {
-//   $("#filterButton").click(function(e) {
-//     e.preventDefault();
-//     // console.log( $( "#searchBox" ).val() );
-//     checkInputAtSearchBox($( "#searchBox" ).val());
-//   });
-// });
+"use strict"
+
+class Solution {
+	constructor(courseName, year, type, professor, author, fileId){
+		this.courseName = courseName;
+		this.year = year;
+		this.type = type;
+		this.professor = professor;
+		this.author = author;
+		this.fileId = fileId;
+	}
+}
+
+let numberOfSolutions = 0; // total number of solution
+
+let solutionList = []
+
+let Solution1 = new Solution("CSC309", 2016, "Midterm", "Ken Jackson", "jellycsc", "solution.html");
+let Solution2 = new Solution("CSC309", 2017, "Midterm", "Ken Jackson", "Cosmos", " ");
+let Solution3 = new Solution("CSC309", 2017, "Midterm", "David Liu", "Claire", " ");
+let Solution4 = new Solution("CSC411", 2016, "Midterm", "Ken Jackson", "17Singer", " ");
+solutionList.push(Solution1);
+solutionList.push(Solution2);
+solutionList.push(Solution3);
+solutionList.push(Solution4);
+
+// const tableContainer = document.querySelector('#displayTableContainer')
+// const table = tableContainer.getElementsByClassName("table table-striped")[0]
+// const tableBody = table.getElementsByClassName('tableBody')[0]
+// console.log(tableBody)
+
+function removeSolutionFromTable(solution) {
+	// Add code here
+	const tableContainer = document.querySelector('#displayTableContainer')
+	const table = tableContainer.getElementsByClassName("table table-striped")[0]
+	const tableBody = table.getElementsByClassName('tableBody')[0]
+	// console.log(tableBody)
+	
+	const rowNum = tableBody.getElementsByTagName('tr').length
+	for (let i = 0; i < rowNum; i++) {
+		// console.log(tableBody.getElementsByTagName('tr')[i]);
+		let user = tableBody.getElementsByTagName('tr')[i].getElementsByTagName('td')[1].textContent
+		if (user === solution.author) { 
+			tableBody.deleteRow(i)
+		}
+
+	}
+}
+
+const deleteButton = document.querySelector('.delete-row');
+// deleteButton.addEventListener('click', removeSolutionFromTable(Solution1));
 
 
-// function checkInputAtSearchBox(courseName){
-// 	const dept = courseName.slice(0,3);
-// 	const courseNumber = courseName.slice(3);
 
-// 	if(isLetter(dept) && isThreeDigits(courseNumber)){
-// 		// const resultCourse = new Course(dept.toUpperCase(), courseNumber);
-// 		console.log(dept.toUpperCase()+courseNumber);
-// 	}
-// }
+
+// console.log(deleteButton)
+// let table = $('#tableBody')
+// console.log(table)

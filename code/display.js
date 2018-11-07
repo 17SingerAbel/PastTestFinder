0adcda973655bc38eea073f6122bb56f0eeb1ab0
Abel Sang <abelsang9683@gmail.com>
2018-11-07 11:29:30 -0500

    can use delete button

--- code/display.js
+++ code/display.js
@@ -11,11 +11,11 @@ class Solution {
 	}
 }
 
-let numberOfSolutions = 0; // total number of solution
+let numberOfSolutions = 4; // total number of solution
 
 let solutionList = []
 
-let Solution1 = new Solution("CSC309", 2016, "Midterm", "Ken Jackson", "jellycsc", "solution.html");
+let Solution1 = new Solution("CSC309", 2016, "Midterm", "Ken Jackson", "jellycsc", "so");
 let Solution2 = new Solution("CSC309", 2017, "Midterm", "Ken Jackson", "Cosmos", " ");
 let Solution3 = new Solution("CSC309", 2017, "Midterm", "David Liu", "Claire", " ");
 let Solution4 = new Solution("CSC411", 2016, "Midterm", "Ken Jackson", "17Singer", " ");
@@ -24,35 +24,43 @@ solutionList.push(Solution2);
 solutionList.push(Solution3);
 solutionList.push(Solution4);
 
-// const tableContainer = document.querySelector('#displayTableContainer')
-// const table = tableContainer.getElementsByClassName("table table-striped")[0]
-// const tableBody = table.getElementsByClassName('tableBody')[0]
-// console.log(tableBody)
+const tableContainer = document.querySelector('#displayTableContainer')
+const table = tableContainer.getElementsByClassName("table table-striped")[0]
+const tableBody = table.getElementsByClassName('tableBody')[0]
+
+tableContainer.addEventListener('click', deleteSolutionFromTable);
+
+function searchUsername(username) {
+	for (let i=0; i < numberOfSolutions; i++) {
+		if (username == solutionList[i].author) {
+			return solutionList[i]
+		}
+	}
+}
+
+function deleteSolutionFromTable(e){
+	e.preventDefault();
+	if(e.target.classList.contains("delete-row")) {
+		let user = e.target.parentElement.parentElement.getElementsByTagName('td')[1].textContent;
+		const solution = searchUsername(user)
+		removeSolutionFromTable(solution)
+	}
+
+}
+
 
 function removeSolutionFromTable(solution) {
-	// Add code here
 	const tableContainer = document.querySelector('#displayTableContainer')
 	const table = tableContainer.getElementsByClassName("table table-striped")[0]
 	const tableBody = table.getElementsByClassName('tableBody')[0]
-	// console.log(tableBody)
-	
-	const rowNum = tableBody.getElementsByTagName('tr').length
+	let rowNum = tableBody.getElementsByTagName('tr').length
 	for (let i = 0; i < rowNum; i++) {
-		// console.log(tableBody.getElementsByTagName('tr')[i]);
-		let user = tableBody.getElementsByTagName('tr')[i].getElementsByTagName('td')[1].textContent
+		let user = tableBody.getElementsByTagName('tr')[i].getElementsByTagName('td')[1].textContent;
 		if (user === solution.author) { 
 			tableBody.deleteRow(i)
+			rowNum -= 1;
+
 		}
 
 	}
 }
-
-const deleteButton = document.querySelector('.delete-row');
-// deleteButton.addEventListener('click', removeSolutionFromTable(Solution1));
-
-
-
-
-// console.log(deleteButton)
-// let table = $('#tableBody')
-// console.log(table)

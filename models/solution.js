e9df697b42497e060b5fc0275a9930509dd3c766
Abel Sang <abelsang9683@gmail.com>
2018-12-06 12:58:39 -0500

    add comment schema

--- models/solution.js
+++ models/solution.js
@@ -1,6 +1,16 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
+const CommentsSchema = new Schema({
+    username: {
+        type: String
+    },
+    context:{
+        type: String
+    }
+    // time: {
+    //     type: Number
+    // }
+});
 const SolutionSchema = new Schema({
     dept: {
         type: String
